c54d5abbf19fa9221e6cf3855f3473d318077a3a
muhammadasman <muhasmangit@gmail.com>
2025-01-14 11:35:49 +0700

    fix: handle create template prod or development

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,8 @@ import { createTemplate } from "./commands/create-template";
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = createTemplate();
+  require('dotenv').config({path: __dirname + '/../.env'});
 
   context.subscriptions.push(disposable);
 }
-export function deactivate() {}
+export function deactivate() { }
